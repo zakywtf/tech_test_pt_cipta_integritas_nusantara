287458c1df38cadb54c39e97d6c6ac4735319047
zakywtf <novazaky1@gmail.com>
2023-02-11 00:09:33 +0700

    Feat: Add Service

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -9,16 +9,49 @@ const IndexController = {
 
     index: async (req, res) => {
         const services = await CatServices.find({ isDeleted: false })
+        const active_menu = 'home'
+        const head_title = 'Beehive Drones: Best Drone System Provider Company'
 
-        res.render('public/home/index', { services });
-        // res.render('notfound')
+        res.render('public/home/index', { head_title, active_menu, services });
     },
 
     service: async (req, res) => {
+        const services = await CatServices.find({ isDeleted: false })
+        const active_menu = 'service'
+        switch (req.params.category) {
+            case 'agriculture':
+                var head_title = 'Best Drone Service for Agriculture | Beehive Drones'
+                break;
+            case 'forestry':
+                var head_title = 'Best Drone Service for Forestry | Beehive Drones'
+                break;
+            case 'logistics':
+                var head_title = 'Best Drone Service for Navier | Beehive Drones'
+                break;
+        
+            default:
+                break;
+        }
 
-        // res.render('public/home/index', { services });
-        // res.render('notfound')
+        res.render('public/service/' + req.params.category, { head_title, active_menu, services });
     },
+
+    blog: async (req, res) => {
+        const services = await CatServices.find({ isDeleted: false })
+        const active_menu = 'blog'
+        const head_title = 'Information About Technology and Business | Beehive Drones'
+
+        res.render('public/blog/index', { head_title, active_menu, services });
+    },
+
+    career: async (req, res) => {
+        const services = await CatServices.find({ isDeleted: false })
+        const active_menu = 'career'
+        const head_title = 'Beehivedrones Job Vacancies, Check Out the Position!'
+
+        res.render('public/career/index', { head_title, active_menu, services });
+    },
+
 
     ping: (req, res) => {
         return apiResponse.successResponse(res, 'Pong');
